c03b595299ad76831ed29c3440597807df2623c7
univertaavi-khk <univer.taavi@khk.ee>
2021-09-24 11:39:51 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// MUUTUJAD
+/*
+SEE
+ON
+MITME
+REALINE
+COMMENT
+*/
+
+// SEE ON MUUTUJA
+let firstName = "Kadi";
+
+// SEE ON KONSTANT
+const surName = "Tamm";
+
+console.log(firstName, surName);
