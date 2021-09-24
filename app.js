d9d2b3e9b3b901adc8753cdddd641a29e331d008
univertaavi-khk <univer.taavi@khk.ee>
2021-09-24 11:41:00 +0300

    first commit222

--- app.js
+++ app.js
@@ -13,4 +13,13 @@ let firstName = "Kadi";
 // SEE ON KONSTANT
 const surName = "Tamm";
 
+// MASSIIVID
+const numbers = [1, 2, 3, 4];
+numbers.push(6);
+
+const person = {
+	name: firstName
+	age: 20
+};
+
 console.log(firstName, surName);
---
+++ app2.js
@@ -0,0 +1,54 @@
+// ANDMETÜÜBID
+
+// PRIMITIVES
+
+// TEKST
+const name = "Kadi Tamm";
+
+console.log(name);
+console.log(typeof name);
+
+// NUMBRID
+const age = 15;
+const height = 1.75;
+
+console.log(age);
+console.log(typeof height);
+
+// TÕEVÄÄRTUSED
+const hasKids = false;
+
+
+console.log(hasKids);
+console.log(typeof hasKids);
+
+// NULL
+const hasAirplane = null;
+
+let test;
+
+console.log(test);
+console.log(typeof test);
+
+// REFERENCE TYPES
+const hobbies = ["music", "sport"];
+
+console.log(hobbies);
+console.log(typeof hobbies);
+
+// OBJEKT
+const person = {
+	name: name
+	age: age
+	height: height
+	hasKids: hasKids
+	hobbies: hobbies
+};
+
+console.log(person);
+console.log(typeof person)
+
+const today = new Date();
+
+console.log(today);
+console.log(typeof today);
