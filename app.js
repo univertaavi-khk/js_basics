ae38d1459a6e14a205a05c861c7717e7a9f7a5ee
univertaavi-khk <univer.taavi@khk.ee>
2021-09-27 14:49:27 +0300

    Conditions

--- app.js
+++ app.js
@@ -1,9 +1,47 @@
-const today = new Date();
+const id = 100;
 
-val = today.getMonth();
-birthday = new Date("10/02/2021 12:00:50");
-birthday.setMonth(1);
-birthday.setDate(28);
 
-console.log(birthday);
-console.log(val);
+// võrdub - ==
+// võrdub + tüübikontroll - ===
+// ei võrdu - !=
+// ei võrdu + tüübikontroll - !==
+
+
+/*if(id == 100){
+    console.log("OK")
+} else {
+    console.log("NOT OK")
+};
+*/
+
+let color = "greeen";
+
+if(color==="red"){
+    console.log("Stop!");
+} else if(color==="yellow"){
+    console.log("Attention!");
+} else if(color==="green"){
+    console.log("Go!");
+} else {
+    console.log("Invalid color!");
+}
+
+// and - &&
+// or - ||
+
+let name = "Kadi";
+let age = 55;
+
+if(age > 0 && age <= 12){
+    console.log(`${name} on laps`);
+} else if(age >= 13 && age < 18) {
+    console.log(`${name} on nooruk`);
+} else {
+    console.log(`${name} on täiskasvanu`);
+}
+
+if(age < 18 || age > 65) {
+    console.log(`${name} ei saa maratonile registreerida`);
+} else {
+    console.log(`${name} saab maratonile registreerida`);
+}
