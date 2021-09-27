85a4f98fbd32c7f2cc119c5e48dac007279ab1c8
univertaavi-khk <univer.taavi@khk.ee>
2021-09-27 15:32:31 +0300

    Tsüklid

--- app.js
+++ app.js
@@ -1,56 +1,21 @@
-let color = "red";
-
-switch(color){
-    case "red":
-        console.log("Stop!");
-        break;
-
-    case "yellow":
-        console.log("Attention!");
-        break;
-
-    case "green":
-        console.log("Go!");
-        break;
-    default:
-        console.log("This color is not allowed!");
+for(let i = 0; i < 10; i++){
+    console.log(i);
 }
 
-const day = new Date();
-
-let weekday = String(day.getDay());
-
-switch(weekday){
-    case "1":
-        console.log("Esmaspäev");
-        break;
-
-    case "2":
-        console.log("Teisipäev");
-        break;
-
-    case "3":
-        console.log("Kolmapäev");
-        break;
-
-    case "4":
-        console.log("Neljapäev");
-        break;
-
-    case "5":
-        console.log("Reede");
-        break;
-
-    case "6":
-        console.log("Laupäev");
-        break;
-
-    case "7":
-        console.log("Pühapäev");
-        break;
-
-    default:
-        console.log("See pole nädalapäev");
-        break;
+for(let i = 0; i < 10; i++){
+    console.log(i);
+    if(i === 5) {
+        continue;
+    }
 }
-console.log(day.getDay());
+
+let i = 0;
+while(i < 10){
+    console.log(i);
+    i++;
+}
+
+const cars = ["Ford", "Toyota", "Mercedes-Benz"];
+for(let i = 0; i < cars.length; i++){
+    console.log[i];
+}
