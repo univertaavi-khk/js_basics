34e0376d18cc8350ecaf4d24645dc8cc78d2f450
univertaavi-khk <univer.taavi@khk.ee>
2021-10-04 13:20:38 +0300

    JS Events - keyboard events

--- app.js
+++ app.js
@@ -3,13 +3,15 @@
 const form = document.querySelector("form");
 const taskInput = document.querySelector("#task")
 
-form.addEventListener("submit", runEvent)
+form.addEventListener("keydown", runEvent);
+form.addEventListener("keyup", runEvent);
+form.addEventListener("focus", runEvent);
+form.addEventListener("blur", runEvent);
+form.addEventListener("cut", runEvent);
+form.addEventListener("paste", runEvent);
+form.addEventListener("input", runEvent);
 
 function runEvent(e) {
     console.log(`Event is ${e.type}`);
-    console.log(taskInput.value)
-    taskInput.value = "";
-    e.preventDefault();
+    console.log(e.target.value);
 }
-
-console.log(taskInput);
